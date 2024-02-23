26167ae932c5366e88759e3716cc42bb24928577
Fer Aguirre <77854667+fer-aguirre@users.noreply.github.com>
2024-02-23 11:28:39 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Travesticidios Argentina',
+    version='0.1.0',
+    author='Fer Aguirre',
+    description='Análisis de datos sobre fallos judiciales de travesticidios en Argentina de 201023',
+    python_requires='>=3',
+    license='MIT License',
+    packages=find_packages(),
+)
